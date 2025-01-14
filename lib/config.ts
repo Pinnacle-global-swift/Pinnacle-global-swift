61744c4e814df83ac1c99662525122baad29bfa6
TEMPLE VOKE <62543994+temple7447@users.noreply.github.com>
2025-01-14 11:25:04 +0100

    sds

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,3 @@
 // export const API_BASE_URL = 'http://localhost:3001/api';
-export const API_BASE_URL = 'https://project-h66a.onrender.com/api';
+// export const API_BASE_URL = 'https://project-h66a.onrender.com/api';
+export const API_BASE_URL = 'https://pinnacle-backend-9itg.onrender.com/api';
