df3e99b9a073410a73f351a5c1e23c614ed508ba
TEMPLE VOKE <62543994+temple7447@users.noreply.github.com>
2025-02-15 14:25:50 +0100

    new updates.sdsdsd

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:3001/api';
-// export const API_BASE_URL = 'https://pinnacle-backend-9itg.onrender.com/api';
+// export const API_BASE_URL = 'http://localhost:3001/api';
+export const API_BASE_URL = 'https://pinnacle-backend-9itg.onrender.com/api';
